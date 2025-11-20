d42d42016fb8250c58f8d950f02ca8bc076c0625
Matthew Gibbons <matthew@southwestkoala.co.uk>
2025-11-20 22:17:20 +0000

    Add keyrings directory creation and update GitHub CLI source list in on-create.sh

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,6 +2,8 @@
 apt update
 apt install ca-certificates curl
 
+mkdir -p -m 755 /etc/apt/keyrings
+   
 curl -fsSL https://pkgs.zabbly.com/key.asc -o /etc/apt/keyrings/zabbly.asc
 sh -c 'cat <<EOF > /etc/apt/sources.list.d/zabbly-incus-stable.sources
 Enabled: yes
@@ -14,6 +16,12 @@ Signed-By: /etc/apt/keyrings/zabbly.asc
 
 EOF'
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg -o /etc/apt/keyrings/githubcli-archive-keyring.gpg
+sh -c 'cat <<EOF > /etc/apt/sources.list.d/github-cli.list
+deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main
+
+EOF'
+
 apt-get update
-apt-get install -y incus-client 
+apt-get install -y gh incus-client 
 
